6be526cccfdb543586f2753f01b65e852715aa4c
Aftar Fadilah <aftar.fadilah@nosc.ai>
2023-09-23 07:25:29 +0700

    fix type

--- src/app/modules/footer/index.tsx
+++ src/app/modules/footer/index.tsx
@@ -30,7 +30,7 @@ const navigation = {
     {
       name: "Facebook",
       href: "#",
-      icon: (props) => (
+      icon: (props: any) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -78,7 +78,7 @@ const navigation = {
     {
       name: "YouTube",
       href: "#",
-      icon: (props) => (
+      icon: (props: any) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
